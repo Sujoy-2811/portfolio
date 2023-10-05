158c8fd6cf0906f54558d9a1c1a516841d521478
Sujoy Manna <62413879+Sujoy-2811@users.noreply.github.com>
2023-10-05 22:47:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import Header from "./components/header/header";
 // import Contact from "./components/main/contact";
-import Main from "./components/main/main";
+import Main from "./components/main/Main";
 
 function App() {
   return (
