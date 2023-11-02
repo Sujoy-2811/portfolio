d55a395c07dbd87ca11b6b6b2c758c684ff6af39
Sujoy Manna <sujoymanna007@gmail.com>
2023-11-02 22:46:20 +0530

    minor update

--- src/components/utils/Skill.jsx
+++ src/components/utils/Skill.jsx
@@ -5,7 +5,7 @@ const Skill = ({ value, skill }) => {
     <div className=" m-6 flex flex-col justify-center items-start gap-2">
       <h4>{skill}</h4>
       <div className="flex gap-2 items-center">
-        <progress value={value} max="100" className=" w-52" />
+        <progress value={value} max="100" className=" w-64" />
         <span>{value + "%"}</span>
       </div>
     </div>
