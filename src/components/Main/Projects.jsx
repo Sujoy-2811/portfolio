d78fce7d2f850545f89f0a495cfc530e554bf3b9
Sujoy Manna <sujoymanna007@gmail.com>
2024-02-14 21:08:34 +0530

    fix: margin in projects section

--- src/components/Main/Projects.jsx
+++ src/components/Main/Projects.jsx
@@ -8,10 +8,10 @@ const Projects = () => {
   return (
     <section
       id="projects"
-      className=" text-center mt-8  mx-6 sm:mt-24 sm:pt-4   pb-7 w-[95%]"
+      className=" text-center mt-8  mx-6 sm:mt-24 sm:pt-4   pb-7 ]"
     >
       <h2 className="text-2xl font-semibold mb-10 sm:text-3xl ">Projects</h2>
-      <ul className=" flex flex-col flex-wrap gap-y-7 justify-center items-center sm:mx-12 list-none sm:flex-row sm:justify-around  ">
+      <ul className=" flex flex-col  gap-y-7 justify-center items-center sm:mx-12 list-none sm:flex-row sm:justify-around  ">
         {projectList.map((item) => (
           <Project key={item.title} props={item} />
         ))}
